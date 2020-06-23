c7aaa593670f2ec79731d28b56c2f863c6cb7dc8
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-23 20:19:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const returnFirstTwoDrivers = function(drivers) {
   let firstDrivers = [drivers[0], drivers[1]];
   return firstDrivers;
 };
+
+const returnLastTwoDrivers = function(drivers) {
+  let lastDrivers = [drivers[-1], drivers[-2]];
+  return lastDrivers;
+};
