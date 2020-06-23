1a5b1b955964393217fbfe96fe4b5a830b98fc5e
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2020-06-23 20:31:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 const selectDifferentDrivers = function(drivers, driverFunctions) {
-  return 
-}
+  return drivers(driverFunctions);
+};
